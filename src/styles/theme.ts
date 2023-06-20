014980056a684e219a627a2699faef8246d588d2
Guilherme Reis <guilherme.reis@localiza.com>
2023-06-20 17:43:29 -0300

    styles: global styles

--- src/styles/theme.ts
+++ src/styles/theme.ts
@@ -9,10 +9,67 @@ const theme = extendTheme({
           solidHoverBg: "#cc2843",
           solidActiveBg: "#b3243a",
         },
+        background: {
+          surface: "#0f1923",
+          backdrop: "rgba(12,21,29,.78)",
+        },
+        text: {
+          primary: "#797b95",
+        },
+        neutral: {
+          outlinedColor: "#bcbfc2",
+        },
       },
     },
     dark: {
       palette: {},
+    },
+  },
+  components: {
+    JoyButton: {
+      defaultProps: {
+        size: "lg",
+      },
+      styleOverrides: {
+        root: {
+          fontSize: "14px",
+          fontWeight: "bold",
+        },
+      },
+    },
+    JoyFormLabel: {
+      styleOverrides: {
+        root: {
+          fontSize: "10px",
+          fontWeight: "bold",
+        },
+      },
+    },
+    JoyInput: {
+      defaultProps: {
+        size: "lg",
+      },
+      styleOverrides: {
+        root: {
+          color: "#ffffff",
+
+          ":hover": {
+            color: "#ffffff",
+          },
+        },
+        input: {
+          "::placeholder": {
+            color: "#fff",
+          },
+        },
+      },
+    },
+    JoySelect: {
+      styleOverrides: {
+        root: {
+          minHeight: "3rem",
+        },
+      },
     },
   },
 });
